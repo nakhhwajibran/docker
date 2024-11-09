c127b671e2500cc0a45dcc9e4626c80ee43e3392
Jibran Nakhwa <nakhwa.jibrann@gmail.com>
2024-11-09 17:33:58 -0500

    My Docker First Project

---
+++ config/settings.py
@@ -0,0 +1,9 @@
+SECRET_KEY="Faraza@1611"
+
+APP_NAME="Moby Dock"
+
+DEBUG=True
+TESTING=False
+
+SQLALCHEMY_DATABASE_URI = "postgresql://mobydock:farzana1611@postgres:5432/mobydock"
+REDIS_URL = "redis://redis:6379/0"
